c5396c8d1c41bc193428239bfe26ef372bef0b74
KUS MAURYA <utsav.singh0023@gmail.com>
2020-06-26 22:56:39 +0530

    daily_login_bonus timer not added

--- app/src/main/java/com/infinity_loop/dunamiclayout/Daily_login_bonus.kt
+++ app/src/main/java/com/infinity_loop/dunamiclayout/Daily_login_bonus.kt
@@ -5,12 +5,17 @@ import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 class Daily_login_bonus : AppCompatActivity() {
 
     var showedToday = false
+    lateinit var time_spent:Button
+
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,9 +48,34 @@ class Daily_login_bonus : AppCompatActivity() {
         else{
             Toast.makeText(this, "Reward received today",Toast.LENGTH_SHORT).show()
         }
+
+
+        // TODO: 26-06-2020 make new thread for timer --->
+        time_spent = findViewById<Button>(R.id.btn_time_spent)
+
+        time_spent.setOnClickListener(View.OnClickListener { v ->
+            MyCounter(1000*60*5, 1000).start()
+        })
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    inner class MyCounter : CountDownTimer {
+        constructor(millisInFuture: Long, interval: Long) : super(
+            millisInFuture,
+            interval
+        )
+
+
+        override fun onFinish() {
+            // TODO: on finish of timer of daily earn
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            time_spent.text = millisUntilFinished.toString()
+        }
+
     }
 }
